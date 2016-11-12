ad84bf3559f376ab6f302d4db76656db0717971d
Elliott Sobek <elliottsobek@gmail.com>
2016-11-11 17:06:34 -0700

    Move GameBoard test files to separate package

---
+++ TermProject/test/GameBoardVariants/GameBoardFreeParking.java
@@ -0,0 +1,15 @@
+package termproject;
+
+public class GameBoardFreeParking extends GameBoard { // Not used?
+
+    public GameBoardFreeParking() {
+        super();
+        JailCell jail = new JailCell();
+        FreeParkingCell freeParking = new FreeParkingCell();
+        GoToJailCell goToJail = new GoToJailCell();
+        super.addCell(jail);
+        super.addCell(freeParking);
+        super.addCell(goToJail);
+
+    }
+}
